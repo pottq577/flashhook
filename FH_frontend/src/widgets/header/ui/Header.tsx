11ea3506d6c03f9a190e927013dc6d83c3e29299
hyun2y00 <pottq577@naver.com>
2026-06-21 16:39:51 +0900

    fix(header): 로고 img alt를 빈 문자열로 변경
    링크 내부에 'FlashHook' 텍스트가 이미 있어 스크린리더에서
    alt 텍스트가 중복 발화될 수 있음. 장식 이미지로 처리.

--- FH_frontend/src/widgets/header/ui/Header.tsx
+++ FH_frontend/src/widgets/header/ui/Header.tsx
@@ -5,7 +5,7 @@ function Header() {
   return (
     <header className={styles.header}>
       <Link to="/" className={styles.logo}>
-        <img src="/favicon.svg" alt="FlashHook 로고" className={styles.logoIcon} />
+        <img src="/favicon.svg" alt="" className={styles.logoIcon} />
         FlashHook
       </Link>
       <nav className={styles.nav}>
