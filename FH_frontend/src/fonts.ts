1ed5a44db71855cc68771adfcae36ddeea181e2d
hyun2y00 <pottq577@naver.com>
2026-07-03 14:19:35 +0900

    refactor(style): 불필요한 JetBrains Mono 폰트 의존성 제거
    이미 전역 Monospace 폰트로 Geist Mono가 지정되어 있음에도 불구하고 역할이 겹치는 JetBrains Mono 폰트 3종이 추가로 로드되어 네트워크 대역폭을 낭비하고 있었습니다.
    불필요한 JetBrains Mono 폰트를 패키지에서 제거하고, import 문 및 CSS 변수에서 삭제했습니다.

--- FH_frontend/src/fonts.ts
+++ FH_frontend/src/fonts.ts
@@ -1,7 +1,4 @@
 import "pretendard/dist/web/variable/pretendardvariable-dynamic-subset.css";
-import "@fontsource/jetbrains-mono/400.css";
-import "@fontsource/jetbrains-mono/500.css";
-import "@fontsource/jetbrains-mono/700.css";
 import "@fontsource/geist-sans/400.css";
 import "@fontsource/geist-sans/500.css";
 import "@fontsource/geist-sans/600.css";
